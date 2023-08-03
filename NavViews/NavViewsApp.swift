c17d5ee0f7d3fbfa9cc4d9a7f7ba7e00748ae0ed
LauYann <metalyanni@gmail.com>
2023-08-03 13:56:44 -0600

    Initial Commit

---
+++ NavViews/NavViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavViewsApp.swift
+//  NavViews
+//
+//  Created by MacBook 26 on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NavViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
