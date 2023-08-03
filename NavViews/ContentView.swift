608fa809325bda15f59fa7520c532d6218c5c91a
XxJohnLCxX <john_chavo@hotmail.com>
2023-08-03 17:19:57 -0600

    tercera prueba John

--- NavViews/ContentView.swift
+++ NavViews/ContentView.swift
@@ -41,7 +41,7 @@ struct ContentView: View {
                         
                     }
                     .sheet(isPresented: $isShowingSecondView) {
-                        infodos(isPresente: $isShowingSecondView)
+                        Info(isPresented: $isShowingSecondView)
                     }
                 
                 
