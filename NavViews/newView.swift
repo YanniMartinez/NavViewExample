89095dcf0b773e9e7f5fa14cf7308c8c002a720e
newN <newN@gmil.com>
2023-08-04 15:23:57 -0600

    add text and new view

---
+++ NavViews/newView.swift
@@ -0,0 +1,20 @@
+//
+//  newView.swift
+//  NavViews
+//
+//  Created by Alumno on 04/08/23.
+//
+
+import SwiftUI
+
+struct newView: View {
+    var body: some View {
+        Text("test").font(.title)
+    }
+}
+
+struct newView_Previews: PreviewProvider {
+    static var previews: some View {
+        newView()
+    }
+}
